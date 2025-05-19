a3783c0362cb8a4e4d241a3fa50eaac5ca918800
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-05-19 12:00:49 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AdminTemplateComponent } from './components/admin-template/admin-template.component';
+import { authenticationGuard } from './guards/authentication.guard';
+import { NotAuthorizedComponent } from './components/not-authorized/not-authorized.component';
+import { RemboursementService } from './services/remboursement.service';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: "/login", pathMatch: "full" },
+  { 
+     path: 'admin',
+    component: AdminTemplateComponent,
+    canActivate: [authenticationGuard],
+    children: [ 
+      { path: 'notAuthorized', component: NotAuthorizedComponent },
+      {  path: 'remboursements', component: RemboursementService },
+    ]
+  }
+];
