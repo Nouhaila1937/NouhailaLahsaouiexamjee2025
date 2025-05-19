2454ef94824628e8556aee689d0dc9c316e95f81
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-05-19 10:04:10 +0100

    test de la couche dao

--- src/main/java/org/example/exam2025/Exam2025Application.java
+++ src/main/java/org/example/exam2025/Exam2025Application.java
@@ -1,7 +1,19 @@
 package org.example.exam2025;
 
+import org.example.exam2025.Entities.Client;
+import org.example.exam2025.Entities.CreditProfessionnel;
+import org.example.exam2025.Entities.Remboursement;
+import org.example.exam2025.Enum.TypeRemboursement;
+import org.example.exam2025.Enum.Typebien;
+import org.example.exam2025.Repositories.ClientRepository;
+import org.example.exam2025.Repositories.CreditProfessionnelRepository;
+import org.example.exam2025.Repositories.RemboursementRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class Exam2025Application {
@@ -10,4 +22,29 @@ public class Exam2025Application {
         SpringApplication.run(Exam2025Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner initData(ClientRepository clientRepository , RemboursementRepository remboursementRepository , CreditProfessionnelRepository creditProfessionnelRepository) {
+        return args -> {
+
+            Remboursement remboursement = new Remboursement();
+            remboursement.setDate(new Date());
+            remboursement.setMontant(1000.0);
+            remboursement.setType(TypeRemboursement.REMBOURSEMENT);
+            remboursementRepository.save(remboursement);
+
+            // Crée et sauvegarde d'abord le client
+            Client client = new Client();
+            client.setNom("Nouhaila");
+            client.setEmail("nouhaila@gmail.com");
+            Client savedClient = clientRepository.save(client);
+
+            CreditProfessionnel cpro = new CreditProfessionnel();
+            cpro.setMontant(1000.0);
+            cpro.setMotif("motif");
+            cpro.setClient(savedClient); // Utilise le client sauvegardé
+            cpro.setRaisonSociale("raison sociale");
+            cpro.setDuree(12);
+            creditProfessionnelRepository.save(cpro);
+        };
+    }
 }
