26af7f2d2d99a6a753482b91b7535dcce3d1b5fe
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-05-19 08:54:55 +0100

    first commit

---
+++ src/main/java/org/example/exam2025/Exam2025Application.java
@@ -0,0 +1,13 @@
+package org.example.exam2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exam2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/exam2025/Exam2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exam2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
